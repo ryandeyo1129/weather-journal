593c5afd7db34587c9791d1f3dc96741359d1cc4
Ryan Deyo <ryan@f1v.co>
2020-07-09 23:29:57 -0400

    feat(weather-app): functioning basic api calls

--- server.js
+++ server.js
@@ -2,18 +2,40 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
+// Setup and start server
+const port = 3000;
 
-// Setup Server
+app.listen(port, () => console.log(`listening on port ${port}`));
+
+// setup GET route endpoint
+app.get('/all', (req, res) => {
+  res.send(projectData);
+})
+
+// setup POST route endpoint
+app.post('/add', (req, res) => {
+  const newData = {
+    date: req.body.date,
+    temp: req.body.main.temp,
+    feelings: req.body.feelings
+  };
+  projectData = newData;
+})
--- website/app.js
+++ website/app.js
@@ -1,5 +1,66 @@
 /* Global Variables */
+const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = '&appid=f12c63dc510035ad2cbf1b8c2da4a9e2';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// GET request
+const getWeather = async (url, zip, key) => {
+  const response = await fetch(`${url}${zip}${key}`)
+  try {
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.log('error', err);
+  }
+}
+
+// POST request
+const postData = async (url = '', data = {}) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data)
+  });
+
+  try {
+    const newData = await response.json();
+    return newData;
+  } catch (err) {
+    console.log('error', err);
+  }
+}
+
+// update UI
+const updateUI = async () => {
+  const request = await fetch('/all');
+  try {
+    const allData = await request.json();
+    document.getElementById('date').innerHTML = allData.date;
+    document.getElementById('temp').innerHTML = allData.temp;
+    document.getElementById('content').innerHTML = allData.feelings;
+  } catch (err) {
+    console.log('error', err);
+  }
+}
+
+// Add event listener and chain promises
+document.getElementById('generate').addEventListener('click', (e) => {
+  const newZip = document.getElementById('zip').value;
+  const feelings = document.getElementById('feelings').value;
+  
+  getWeather(baseUrl, newZip, apiKey)
+    .then((data) => {
+      data.date = newDate;
+      data.feelings = feelings;
+      postData('/add', data);
+    })
+    .then(() => {
+      updateUI();
+    });
+})
